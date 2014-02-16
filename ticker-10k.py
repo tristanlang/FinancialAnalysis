3ba3e7278b14bba15fbd8bd6d84b41ff7138f9da
tristanjlang <tristan@alum.mit.edu>
2014-02-15 20:33:07 -0500

    added note about having to update text in opening zip contents for the ticker of the filing i am looking at

--- ticker-10k.py
+++ ticker-10k.py
@@ -2,6 +2,7 @@ from xml.dom import minidom
 import urllib.request
 import zipfile
 from io import BytesIO
+import requests
 
 # from the SEC RSS XML, get the zip files associated with the most recent filings
 contents = urllib.request.urlopen('https://www.sec.gov/Archives/edgar/usgaap.rss.xml').read()
@@ -11,9 +12,10 @@ filings = d.getElementsByTagName('item')
 for filing in filings:
     period = filing.getElementsByTagName('edgar:period')[0].firstChild.nodeValue
     zipurl = filing.getElementsByTagName('enclosure').item(0).getAttribute('url')
-    zipcontents = urllib.request.urlopen(zipurl)
-    z = zipfile.ZipFile(StringIO(r.content)))
+    zipcontents = requests.get(zipurl)
+    z = zipfile.ZipFile(BytesIO(zipcontents.content))
     
+    # need to extract ticker(?)(...i believe tik was one of the tickers i looked at)...from filing, and replace tik
     f = z.open('tik-' + period + '.xml')
     secfiling = f.read() # string containing the full xbrl/xml of the filing
     f.close()
